f930491f932c31c4a99597dc85f2dbc61c2792c9
Faiz <tenkois@gmail.com>
2019-01-09 17:12:57 +0800

    All rspec successfull

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.feature "Listing Articles" do
 
   before do
-    @article1 = Article.create(title: "The first article", body: "lorem ipsum mareng krapu kepunden")
+    @article1 = Article.create(title: "The first article", body: "lorem ipsum dolor sit amet, consectortetur.")
 
-    @article2 = Article.create(title: "The second article", body: "Body of 2nd article ")
+    @article2 = Article.create(title: "The second article", body: "Body of 2nd article")
 
   end
 
